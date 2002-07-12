b0222b6e86d9ece3962c5c78700fcda1ca365738
Tim Jansen <tim@tjansen.de>
2002-07-12 20:25:04 +0000

    Fix: allow vnc urls, the last commit broke them
    svn path=/trunk/kdenetwork/krdc/; revision=166308

--- newconnectiondialog.ui.h
+++ newconnectiondialog.ui.h
@@ -1,6 +1,8 @@
+#include <qregexp.h>
 
 /* Disable connect before user typed something like a host name */
 void NewConnectionDialog::hostChanged(const QString &text) {
-    connectButton->setEnabled((text.length() > 0) && (text!=":"));
+    connectButton->setEnabled(text.contains(QRegExp(":[0-9]+$")) ||
+			      text.contains(QRegExp("^vnc://.+")));
 }
 
