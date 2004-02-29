bf3c50a3ca6bea0d559f44487cd4072d4134f24f
Arend van Beelen jr. <arend@auton.nl>
2004-02-29 17:16:31 +0000

    Backport of fix for bug #69637.
    svn path=/branches/KDE_3_2_BRANCH/kdenetwork/krdc/; revision=292579

--- krdc.cpp
+++ krdc.cpp
@@ -220,6 +220,7 @@ bool KRDC::start()
 	connect(m_keyCaptureDialog, SIGNAL(keyPressed(XEvent*)),
 		m_view, SLOT(pressKey(XEvent*)));
 
+	m_view->grabKeyboard();
 	return m_view->start();
 }
 
@@ -483,7 +484,7 @@ void KRDC::switchToFullscreen(bool scaling)
 	t->insertButton("configure", FS_ADVANCED_ID, m_popup, true, i18n("Advanced options"));
 	KToolBarButton *advancedButton = t->getButton(FS_ADVANCED_ID);
 	QToolTip::add(advancedButton, i18n("Advanced options"));
-	advancedButton->setPopupDelay(0);
+	//advancedButton->setPopupDelay(0);
 
 	QLabel *hostLabel = new QLabel(t);
 	hostLabel->setName("kde toolbar widget");
@@ -598,7 +599,7 @@ void KRDC::switchToNormal(bool scaling)
 		t->insertButton("configure", 3, m_popup, true, i18n("Advanced"));
 		KToolBarButton *advancedButton = t->getButton(3);
 		QToolTip::add(advancedButton, i18n("Advanced options"));
-		advancedButton->setPopupDelay(0);
+		//advancedButton->setPopupDelay(0);
 
 		if (m_layout)
 			delete m_layout;
