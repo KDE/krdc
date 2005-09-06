6802853b4b7d5e862b29e2725de7b388665fd957
Caleb Tennis <caleb@gentoo.org>
2005-09-06 21:19:16 +0000

    Use KKeyNative::modXBlah instead of KKeyNative::modX(ModFlags)
    svn path=/trunk/KDE/kdenetwork/krdc/; revision=457878

--- vnc/kvncview.cpp
+++ vnc/kvncview.cpp
@@ -602,25 +602,25 @@ void KVncView::wheelEvent(QWheelEvent *e) {
 void KVncView::pressKey(XEvent *xe) {
 	KKeyNative k(xe);
 	uint mod = k.mod();
-	if (mod & KKeyNative::modX(KKey::SHIFT))
+	if (mod & KKeyNative::modXShift())
 		m_wthread.queueKeyEvent(XK_Shift_L, true);
-	if (mod & KKeyNative::modX(KKey::CTRL))
+	if (mod & KKeyNative::modXCtrl())
 		m_wthread.queueKeyEvent(XK_Control_L, true);
-	if (mod & KKeyNative::modX(KKey::ALT))
+	if (mod & KKeyNative::modXAlt())
 		m_wthread.queueKeyEvent(XK_Alt_L, true);
-	if (mod & KKeyNative::modX(KKey::WIN))
+	if (mod & KKeyNative::modXWin())
 		m_wthread.queueKeyEvent(XK_Meta_L, true);
 
 	m_wthread.queueKeyEvent(k.sym(), true);
 	m_wthread.queueKeyEvent(k.sym(), false);
 
-	if (mod & KKeyNative::modX(KKey::WIN))
+	if (mod & KKeyNative::modXWin())
 		m_wthread.queueKeyEvent(XK_Meta_L, false);
-	if (mod & KKeyNative::modX(KKey::ALT))
+	if (mod & KKeyNative::modXAlt())
 		m_wthread.queueKeyEvent(XK_Alt_L, false);
-	if (mod & KKeyNative::modX(KKey::CTRL))
+	if (mod & KKeyNative::modXCtrl())
 		m_wthread.queueKeyEvent(XK_Control_L, false);
-	if (mod & KKeyNative::modX(KKey::SHIFT))
+	if (mod & KKeyNative::modXShift())
 		m_wthread.queueKeyEvent(XK_Shift_L, false);
 
 	m_mods.clear();
