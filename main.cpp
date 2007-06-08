4ff163bbde94db8af6aa695843aa869e73dbb96a
Urs Wolfer <uwolfer@kde.org>
2007-06-08 22:13:28 +0000

    SVN_SILENT: fix copy paste mistake in license
    svn path=/branches/work/soc-krdc/krdc/; revision=673071

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ** GNU General Public License for more details.
 **
-** You should have received a copy of the GNU Library General Public License
-** along with this library; see the file COPYING.LIB. If not, write to
+** You should have received a copy of the GNU General Public License
+** along with this program; see the file COPYING. If not, write to
 ** the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ** Boston, MA 02110-1301, USA.
 **
