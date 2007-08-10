e20d06f3a473409c32e983cb891232f598cfe233
Laurent Montel <montel@kde.org>
2007-08-10 20:11:51 +0000

    Fix extract message
    svn path=/trunk/KDE/kdenetwork/krdc/; revision=698716

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 $EXTRACTRC *.ui */*.ui > rc.cpp || exit 11
 $EXTRACTRC *.rc */*.rc >> rc.cpp || exit 12
-$XGETTEXT *.cpp */*.cpp *.h */*.h -o $podir/krdc.pot
+$XGETTEXT *.cpp */*.cpp -o $podir/krdc.pot
+rm -f rc.cpp
