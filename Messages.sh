2aa1b4d7dbcd722e7e139a79b130ad42ca97e925
Allen Winter <winter@kde.org>
2007-12-01 16:12:42 +0000

    append to rc.cpp
    svn path=/trunk/KDE/kdenetwork/krdc/; revision=743655

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-$EXTRACTRC *.ui */*.ui > rc.cpp || exit 11
+$EXTRACTRC *.ui */*.ui >> rc.cpp || exit 11
 $EXTRACTRC *.rc */*.rc >> rc.cpp || exit 12
 $XGETTEXT *.cpp */*.cpp -o $podir/krdc.pot
 rm -f rc.cpp
