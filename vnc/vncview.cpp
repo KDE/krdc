d23ad705aa223e87524afcf5c1aa61e11f14460b
Urs Wolfer <uwolfer@kde.org>
2007-12-03 20:08:04 +0000

    memoryleaks--
    svn path=/trunk/KDE/kdenetwork/krdc/; revision=744496

--- vnc/vncview.cpp
+++ vnc/vncview.cpp
@@ -252,8 +252,8 @@ void VncView::focusOutEvent(QFocusEvent *event)
 //         kDebug(5011) << "event->reason() == Qt::TabFocusReason";
         event->ignore();
         setFocus(); // get focus back and send tab key event to remote desktop
-        keyEvent(new QKeyEvent(QEvent::KeyPress, Qt::Key_Tab, Qt::NoModifier));
-        keyEvent(new QKeyEvent(QEvent::KeyRelease, Qt::Key_Tab, Qt::NoModifier));
+        vncThread.keyEvent(XK_Tab, true);
+        vncThread.keyEvent(XK_Tab, false);
     }
 
     event->accept();
