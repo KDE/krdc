15f894ac4dc6eb4d4ca25684f784fed144ae5c4a
Urs Wolfer <uwolfer@kde.org>
2008-05-20 21:17:54 +0000

    Prevent issue with Qt 4.4 alien widgets and QX11EmbedContainer. Make fullscreentoolbar again working together with RDP. BUG:162308
    svn path=/trunk/KDE/kdenetwork/krdc/; revision=810488

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -511,6 +511,7 @@ void MainWindow::switchFullscreen()
         fullscreenLayout->addWidget(scrollArea);
 
         MinimizePixel *minimizePixel = new MinimizePixel(m_fullscreenWindow);
+        minimizePixel->winId(); // force it to be a native widget (prevents problem with QX11EmbedContainer)
         connect(minimizePixel, SIGNAL(rightClicked()), m_fullscreenWindow, SLOT(showMinimized()));
 
         m_fullscreenWindow->show();
@@ -632,6 +633,7 @@ void MainWindow::showRemoteViewToolbar()
         actionCollection()->action("switch_fullscreen")->setText(i18n("Switch to Window Mode"));
 
         m_toolBar = new FloatingToolBar(m_fullscreenWindow, m_fullscreenWindow);
+        m_toolBar->winId(); // force it to be a native widget (prevents problem with QX11EmbedContainer)
         m_toolBar->setSide(FloatingToolBar::Top);
 
         QLabel *hostLabel = new QLabel(m_remoteViewList.at(m_currentRemoteView)->url().prettyUrl(KUrl::RemoveTrailingSlash), m_toolBar);
