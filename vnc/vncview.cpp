f886df67ce5fb7877610eeabb6246aeecb67bfa0
Urs Wolfer <uwolfer@kde.org>
2008-08-02 14:26:22 +0000

    Backport: SVN commit 841060 by uwolfer:
    Correctely respect key modifiers in VNC.
    Patch by Guillaume Pothier, thanks a lot!
    CCBUG:168015
    CCBUG:162865
    (#162865: fixes KDE 4 issues, other issues listed there are already fixed in KDE 4)
    
    svn path=/branches/KDE/4.1/kdenetwork/krdc/; revision=841061

--- vnc/vncview.cpp
+++ vnc/vncview.cpp
@@ -39,10 +39,18 @@
 #include <QPainter>
 #include <QMouseEvent>
 
+// Definition of key modifier mask constants
+#define KMOD_Alt_R 	0x01
+#define KMOD_Alt_L 	0x02
+#define KMOD_Meta_L 	0x04
+#define KMOD_Control_L 	0x08
+#define KMOD_Shift_L	0x10
+
 VncView::VncView(QWidget *parent, const KUrl &url)
         : RemoteView(parent),
         m_initDone(false),
         m_buttonMask(0),
+        m_modifiersMask(0),
         m_repaint(false),
         m_quitFlag(false),
         m_firstPasswordTry(true),
@@ -444,6 +452,8 @@ void VncView::wheelEvent(QWheelEvent *event)
 
 void VncView::keyEvent(QKeyEvent *e)
 {
+    int mask = 0;
+    
     rfbKeySym k = 0;
     switch (e->key()) {
     case Qt::Key_Backspace: k = XK_BackSpace; break;
@@ -483,30 +493,49 @@ void VncView::keyEvent(QKeyEvent *e)
     case Qt::Key_NumLock: k = XK_Num_Lock; break;
     case Qt::Key_CapsLock: k = XK_Caps_Lock; break;
     case Qt::Key_ScrollLock: k = XK_Scroll_Lock; break;
-    case Qt::Key_Shift: k = XK_Shift_L; break;
-    case Qt::Key_Control: k = XK_Control_L; break;
-    case Qt::Key_AltGr: k = XK_Alt_R; break;
-    case Qt::Key_Alt: k = XK_Alt_L; break;
-    case Qt::Key_Meta: k = XK_Meta_L; break;
+    case Qt::Key_Shift: k = XK_Shift_L; mask |= KMOD_Shift_L; break;
+    case Qt::Key_Control: k = XK_Control_L; mask |= KMOD_Control_L; break;
+    case Qt::Key_AltGr: k = XK_ISO_Level3_Shift; mask |= KMOD_Alt_R; break;
+    case Qt::Key_Alt: k = XK_Alt_L; mask |= KMOD_Alt_L; break;
+    case Qt::Key_Meta: k = XK_Meta_L; mask |= KMOD_Meta_L; break;
     case Qt::Key_Mode_switch: k = XK_Mode_switch; break;
     case Qt::Key_Help: k = XK_Help; break;
     case Qt::Key_Print: k = XK_Print; break;
     case Qt::Key_SysReq: k = XK_Sys_Req; break;
     default: break;
     }
+    
+    // Transform dead keys
+    if (e->key() >= Qt::Key_Dead_Grave && e->key() <= Qt::Key_Dead_Horn) {
+        k = e->key() - Qt::Key_Dead_Grave + XK_dead_grave;
+    }
+    
+    bool pressed = (e->type() == QEvent::KeyPress) ? true : false;
+    m_modifiersMask = pressed ? m_modifiersMask | mask : m_modifiersMask & ~mask;
+
+    bool hasShift = m_modifiersMask & KMOD_Shift_L;
+    bool hasOtherMod = m_modifiersMask & (KMOD_Alt_R | KMOD_Alt_L | KMOD_Meta_L | KMOD_Control_L);
+    bool isUpper = e->key() >= 'A' && e->key() <= 'Z';
+    // bool isLower = e->key() >= 'a' && e->key() <= 'z';
+    // bool isLetter = isLower || isUpper;
 
     if (k == 0) {
-        if (e->key() < 0x100 && e->text().length() > 0)
-            k = QChar(e->text().at(0)).unicode(); //respect upper- / lowercase
-        else
-            rfbClientLog("Unknown keysym: %d\n", e->key());
+        if (hasOtherMod && (! isUpper || hasShift)) {
+            k = e->key();
+        } else {
+            if (e->key() < 0x100 && e->text().length() > 0)
+                k = QChar(e->text().at(0)).unicode(); //respect upper- / lowercase
+            else
+                rfbClientLog("Unknown keysym: 0x%x\n", e->key());
+        }
     }
 
     if (k < 26) // workaround for modified keys by pressing CTRL
         k += 96;
 
-    vncThread.keyEvent(k, (e->type() == QEvent::KeyPress) ? true : false);
+    //rfbClientLog("Key event: orig: 0x%x, sent: 0x%x\n", e->key(), k);
 
+    vncThread.keyEvent(k, pressed);
     RemoteView::keyEvent(e);
 }
 
