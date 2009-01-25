6179e5a8d89b9c99fc556124a62cf023105af128
Urs Wolfer <uwolfer@kde.org>
2009-01-25 15:06:51 +0000

    Disable clipboardsharing in view only mode. CCBUG:170479
    svn path=/trunk/KDE/kdenetwork/krdc/; revision=916570

--- vnc/vncview.cpp
+++ vnc/vncview.cpp
@@ -368,6 +368,8 @@ void VncView::setViewOnly(bool viewOnly)
 {
     RemoteView::setViewOnly(viewOnly);
 
+    m_dontSendClipboard = viewOnly;
+
     if (viewOnly)
         setCursor(Qt::ArrowCursor);
     else
