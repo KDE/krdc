7fd4698930a960150e7750b8917fcf37843e2516
Urs Wolfer <uwolfer@kde.org>
2009-02-22 18:21:19 +0000

    Revert QTimer change from r930130. Stupid change which wont work.
    svn path=/trunk/KDE/kdenetwork/krdc/; revision=930138

--- vnc/vncclientthread.cpp
+++ vnc/vncclientthread.cpp
@@ -160,10 +160,10 @@ VncClientThread::VncClientThread(QObject *parent)
     QMutexLocker locker(&mutex);
     m_stopped = false;
 
-    QTimer outputErrorMessagesCheckTimer;
-    outputErrorMessagesCheckTimer.setInterval(500);
-    connect(&outputErrorMessagesCheckTimer, SIGNAL(timeout()), this, SLOT(checkOutputErrorMessage()));
-    outputErrorMessagesCheckTimer.start();
+    QTimer *outputErrorMessagesCheckTimer = new QTimer(this);
+    outputErrorMessagesCheckTimer->setInterval(500);
+    connect(outputErrorMessagesCheckTimer, SIGNAL(timeout()), this, SLOT(checkOutputErrorMessage()));
+    outputErrorMessagesCheckTimer->start();
 }
 
 VncClientThread::~VncClientThread()
