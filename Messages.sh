1387027b415d115432cb7b20ba294af4ac749814
Andrew Coles <andrew.i.coles@googlemail.com>
2009-04-09 21:39:54 +0000

    SVN_SILENT
    Updated krdc/Messages.sh to pick up the one string from the .kcfg file.
    
    
    svn path=/trunk/KDE/kdenetwork/krdc/; revision=951696

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 $EXTRACTRC *.ui */*.ui >> rc.cpp || exit 11
 $EXTRACTRC *.rc */*.rc >> rc.cpp || exit 12
+$EXTRACTRC */*.kcfg >> rc.cpp
 $XGETTEXT *.cpp */*.cpp -o $podir/krdc.pot
 rm -f rc.cpp
