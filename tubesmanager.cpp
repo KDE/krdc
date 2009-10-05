c358082d2e64e805288c58268b3cedee6d224123
Abner Silva <abner.silva@kdemail.net>
2009-10-05 13:23:47 +0000

    Tp Handler is now also filtering channels by "Requested" property (incoming channels).
    svn path=/trunk/KDE/kdenetwork/krdc/; revision=1031574

--- tubesmanager.cpp
+++ tubesmanager.cpp
@@ -42,10 +42,12 @@ static inline Tp::ChannelClassList channelClassList()
     filter0[TELEPATHY_INTERFACE_CHANNEL ".ChannelType"] =
             QDBusVariant(TELEPATHY_INTERFACE_CHANNEL_TYPE_STREAM_TUBE);
     filter0[TELEPATHY_INTERFACE_CHANNEL_TYPE_STREAM_TUBE ".Service"] = QDBusVariant("rfb");
+    filter0[TELEPATHY_INTERFACE_CHANNEL ".Requested"] = QDBusVariant(false);
 
     filter1[TELEPATHY_INTERFACE_CHANNEL ".ChannelType"] =
             QDBusVariant(TELEPATHY_INTERFACE_CHANNEL_TYPE_TUBES);
     filter1[TELEPATHY_INTERFACE_CHANNEL ".TargetHandleType"] = QDBusVariant(Tp::HandleTypeContact);
+    filter1[TELEPATHY_INTERFACE_CHANNEL ".Requested"] = QDBusVariant(false);
 
     return Tp::ChannelClassList() << Tp::ChannelClass(filter0) << Tp::ChannelClass(filter1);
 }
