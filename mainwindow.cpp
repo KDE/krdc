c1776a88b087b9460a101d2c82cdbfa130537e32
Tony Murray <murraytony@gmail.com>
2010-04-27 17:19:48 +0000

    Return to window mode if there are no more open connections.
    BUG: 235223
    
    svn path=/trunk/KDE/kdenetwork/krdc/; revision=1119700

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -579,8 +579,14 @@ void MainWindow::disconnectHost()
         saveHostPrefs();
     }
 
+    // if closing the last connection, create new connection tab
     if (m_tabWidget->count() == 0) {
         newConnectionPage();
+    }
+
+    // if the newConnectionWidget is the only tab and we are fullscreen, switch to window mode
+    if (m_fullscreenWindow && m_tabWidget->count() == 1  && m_tabWidget->currentWidget() == m_newConnectionWidget) {
+        switchFullscreen();
     }
 }
 
@@ -602,8 +608,14 @@ void MainWindow::closeTab(QWidget *widget)
         widget->deleteLater();
     }
 
+    // if closing the last connection, create new connection tab
     if (m_tabWidget->count() == 0) {
         newConnectionPage();
+    }
+
+    // if the newConnectionWidget is the only tab and we are fullscreen, switch to window mode
+    if (m_fullscreenWindow && m_tabWidget->count() == 1  && m_tabWidget->currentWidget() == m_newConnectionWidget) {
+        switchFullscreen();
     }
 }
 
